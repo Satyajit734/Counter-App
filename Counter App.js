b795b4be706af7ce16867bb2cea0b4618101bddb
Satyajit Samanta <contact.satyajitx@gmail.com>
2026-03-10 10:23:41 +0530

    Add files via upload

---
+++ Counter App.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+    if(count > 0){   
+        count -= 1;
+    }
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
